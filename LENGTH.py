eceb92983ad5de5d1c3048258566c675319e9f36
Koko <59309184+Yuan-KK@users.noreply.github.com>
2021-04-17 10:50:40 +0800

    Update LENGTH.py

--- LENGTH.py
+++ LENGTH.py
@@ -1,9 +1,4 @@
-# C:\Users\admin\Desktop\rotation\Python_assignment\caulobacterNA1000.fasta
-# C:\Users\admin\Desktop\rotation\Python_assignment\EcoliK12.fasta
-# C:\Users\admin\Desktop\rotation\Python_assignment\EcoliO157.fasta
-# C:\Users\admin\Desktop\rotation\Python_assignment\Homo sapiens chromosome 21.fasta
-
-PATH = r"C:\Users\admin\Desktop\rotation\Python_assignment\Homo sapiens chromosome 21.fasta"
+PATH = input("input path:")
 f = open (PATH,"r")
 base_num = 0
 for line in f:
